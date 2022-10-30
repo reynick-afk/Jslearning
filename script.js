22cedbbbed9a92fd768b322eb1ca74f8ac05ff86
Reynick <danilka-123-02@mail.ru>
2022-10-30 22:35:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// console.log(1);
+
+// Практика подключения js к html страницы
+
+// ПЕременные < вкладываем что-либо
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// Прямых переменных в js нет!
+
+
+// alert('Are you here?');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Салам алейкум!",
+//     "Алейкум салам");
+
+// console.log(answer + "salam");
+// console.log(typeof(answer));
+
+
+let answers = [];
+
+answers[0] = prompt('Как вас зовут?', '');
+answers[1] = prompt('Как у вас дела?', '');
+answers[2] = prompt('Кто по жизни?', '');
+
+// document.write(answers);
